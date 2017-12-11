d1fd4b16370a8b3415252e76a8cd149c1282feff
Jaka Prima Maulana <jakaprimamaulana@oprent7.local>
2017-12-11 11:44:30 +0700

    string and list

--- 3string.py
+++ 3string.py
@@ -32,6 +32,17 @@ a[0:2] #ja string ambil 2 data dari index 0
 a[:1] + a[3:] #jo
 
 
+# length
+len(a);
+
+
+#tranverse
+a[1::2] #ao
+a[::-1] #okaj
+
+
+
+
 
 
 
---
+++ 4list.py
@@ -0,0 +1,2 @@
+# list
+# python mengetahui senyawa tipe data, digunakan untuk grupin bersama dengan value lain. paling serbaguna/versatile adalah list, dimana dapat ditulis sebagai list dari pemisahan koma value (item) antara kurung persegi, list mungkin mengandung tipe data berbeda, tetapi biasanya semuanya tipe yang sama
